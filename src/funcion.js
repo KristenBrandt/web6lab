9bf060d573044b48d80611d49b27adaa7b52712e
KristenBrandt <kabrandtf@gmail.com>
2022-03-02 23:48:53 -0600

    Initial commit

---
+++ src/funcion.js
@@ -0,0 +1,4 @@
+function myFunction() {
+   var element = document.body;
+   element.style.background = "blue";
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+alert("Webpack K");
+
+import {myFunction} from "./funcion.js";
+import "./body.css"
+import "./a.css"
+import "./h1.css"
